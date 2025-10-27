71a3f7e660026fa1ffcf31aa9094c2321f8ce9e3
Supa <36031171+0Supa@users.noreply.github.com>
2025-10-27 07:22:47 +0200

    redlib: use prod branch

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     waybar.url = "github:Alexays/Waybar";
     stylix.url = "github:nix-community/stylix/release-25.05";
     technorino.url = "git+https://github.com/2547techno/technorino?submodules=1";
-    redlib.url = "github:0Supa/redlib";
+    redlib.url = "github:0Supa/redlib/prod";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
     uploader-basic.url = "github:0Supa/uploader-basic";
   };
