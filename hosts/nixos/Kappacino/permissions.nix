8c7314d679ac9da0e24860e6ba9f7b2990c23ead
Supa <git@supa.sh>
2025-12-07 20:48:22 +0000

    permissions: always reapply on rebuild

--- hosts/nixos/Kappacino/permissions.nix
+++ hosts/nixos/Kappacino/permissions.nix
@@ -2,13 +2,40 @@
   ...
 }:
 {
-  systemd.tmpfiles.rules = [
-    "Z /mnt/hdd_4t 0775 supa - -"
-    "Z /mnt/hdd_4t/movies 2770 - media -"
-    "Z /mnt/hdd_4t/music 2770 - media -"
-    "Z /mnt/hdd_500g 2770 - media -"
-    "Z /var/www 2770 supa www -"
-    "Z /home/minecraft 2770 - minecraft -"
-    "Z /home/fivem 2770 - fivem -"
-  ];
+  systemd.tmpfiles.settings = {
+    "10-custom-permissions-nix" = {
+      "/mnt/hdd_4t".Z = {
+        mode = "0775";
+        user = "supa";
+      };
+      "/mnt/hdd_4t/movies".Z = {
+        mode = "2770";
+        group = "media";
+      };
+      "/mnt/hdd_4t/music".Z = {
+        mode = "2770";
+        group = "media";
+      };
+      "/mnt/hdd_500g".Z = {
+        mode = "2770";
+        group = "media";
+      };
+      "/var/www".Z = {
+        mode = "2770";
+        user = "supa";
+        group = "www";
+      };
+      "/home/minecraft".Z = {
+        mode = "2770";
+        group = "minecraft";
+      };
+      "/home/fivem".Z = {
+        mode = "2770";
+        group = "fivem";
+      };
+    };
+  };
+
+  # always reapply permissions
+  systemd.services."systemd-tmpfiles-resetup".partOf = [ "sysinit-reactivation.target" ];
 }
