97da87a3ac6e885a335d5b74cf21bbca745ef644
Supa <supa.codes@gmail.com>
2025-12-08 21:08:16 +0200

    nix: remove other substituters

--- common/nix.nix
+++ common/nix.nix
@@ -24,7 +24,6 @@
 
     settings = {
       trusted-users = [
-        "root"
         "@wheel"
       ];
       experimental-features = [
@@ -34,22 +33,6 @@
       auto-optimise-store = true;
       warn-dirty = false;
       builders-use-substitutes = true;
-      substituters = [
-        "https://cache.nixos.org?priority=10"
-        "https://supa.cachix.org"
-        "https://nix-community.cachix.org"
-        "https://nixpkgs-wayland.cachix.org"
-        "https://niri.cachix.org"
-        "https://technorino.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "supa.cachix.org-1:+rC20DiSj3IB8u8LgKV30nYAOwk9vP9aty3rdFz9/YM="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-        "technorino.cachix.org-1:u2mWFvgBuof1W3wr8VT5UQ10m4T1yoJb6fVnmhXf04o="
-      ];
     };
   };
 
