5cb72d99a8b4413e17be7ddf2e06295d25f6fd73
Supa <git@supa.sh>
2025-12-10 07:07:34 +0000

    minecraft: use custom server flags

--- hosts/nixos/Kappacino/services/systemd/default.nix
+++ hosts/nixos/Kappacino/services/systemd/default.nix
@@ -96,8 +96,7 @@ in
         Restart = "always";
         RestartSec = 3;
         WorkingDirectory = "/home/minecraft/supa";
-        # ExecStart = "${lib.getExe' pkgs.jdk21 "java"} -Xmx16G -Xms4G -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar nogui";
-        ExecStart = "${lib.getExe' pkgs.jdk21 "java"} -Xmx8G -Xms2G -jar paper.jar nogui";
+        ExecStart = "${lib.getExe' pkgs.jdk21 "java"} -Xmx8G -Xms2G -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -jar paper.jar nogui";
       };
       wantedBy = [ "multi-user.target" ];
     };
